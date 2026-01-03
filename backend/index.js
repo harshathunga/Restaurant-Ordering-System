fcc74b798de52b099bcea3982924b3cf8ca0f302
harshathunga <76578804+harshathunga@users.noreply.github.com>
2026-01-02 18:58:39 -0600

    \first commit

---
+++ backend/index.js
@@ -0,0 +1,24 @@
+import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+// import jobdata from "./routes/jobdata.js";
+import cors from "cors";
+const app = express();
+const router = express.Router();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["POST", "GET", "DELETE", 'PUT'],
+
+    credentials: true,
+  })
+); 
+
+// app.use("/jobs", jobdata);
+
+app.listen(process.env.port, () => {
+  console.log("server is running in", process.env.port );
+});
